67d490e5909bcb1438d551fcefc7522498bd2666
Usama Ashraf <usama@celerify.com>
2022-07-19 18:22:01 +0500

    left indentation

--- scss-page/src/Section-Left/Left.jsx
+++ scss-page/src/Section-Left/Left.jsx
@@ -44,6 +44,34 @@ const Left = () => {
                         />
                     </Card>
                 </div>
+                <div className="left-content">
+                    <h2><span className='left-name'>Nabeel</span>  Ahmad  00 </h2>
+                    <a href='/' ><i className="fa-solid fa-location-dot"></i>Location area</a>
+                    <a href='/'><i className="fa-solid fa-briefcase"></i>Current Location</a>
+                    <p>Experienced occupation: ---</p>
+                </div>
+                <div className='left-edit'>
+                    <a href='/'><i className="fa-solid fa-pen"></i> edit</a>
+                </div>
+                {/* </section> */}
+                <form action="#">
+                    <div className='left-input-field'>
+                        <input type="text" className='left-input-field' placeholder='Self-introduction' />
+                        <div className='edit'> <a href="/"><i className="fa-solid fa-pen"></i> edit</a></div>
+                        <label htmlFor="self-introduction">Fill in self-introduction</label>
+                    </div>
+                    <div className='left-input-field'>
+                        <input type="text" className='left-input-field' placeholder='Working history' />
+                        <div className='addition'> <a href="/"><i className="fa-solid fa-square-plus"></i> addition</a></div>
+                        <label htmlFor="Add working history">Add working history</label>
+                    </div>
+                    <div className='left-input-field'>
+                        <input type="text" className='left-input-field' placeholder='All qualifications' />
+                        <div className='addition'> <a href="/"><i className="fa-solid fa-square-plus"></i> addition</a></div>
+                        <label htmlFor="Add Qualifications">Add qualification</label>
+                    </div>
+                </form>
+                {/* </div> */}
             </main>
         </>
     );
