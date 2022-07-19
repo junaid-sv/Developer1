1b45292ea23040ce9c3f0d3b6738512e53591ff1
Usama Ashraf <usama@celerify.com>
2022-07-19 18:24:06 +0500

    color change logo

--- scss-page/src/Navbar/Navbar.jsx
+++ scss-page/src/Navbar/Navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = () => {
         <>
         <div className="navbar">
             <a href="/" className='logo'>Wecare</a>
-                <nav className='nav-links'>
-                    <ul>
-                        <li><a href="/">Job search</a></li>
-                        <li><a href="/">be on one's mind!</a></li>
-                        <li><a href="/">message</a></li>
-                        <li><a href="/" className='active'>My page</a></li>
-                        <li><a href="/"><button className='link-button'>N</button></a></li>
-                    </ul>
-                    <div className="logout-content">
-                        <a href="/"><button className='logout-button'>Logout</button></a>
-                    </div>
-                </nav>
-            </div>
+            <nav className='nav-links'>
+                <ul>
+                    <li><a href="/">Job search</a></li>
+                    <li><a href="/">be on one's mind!</a></li>
+                    <li><a href="/">message</a></li>
+                    <li><a href="/" className='active'>My page</a></li>
+                    <li><a href="/"><button className='link-button'>N</button></a></li>
+                </ul>
+                <div className="logout-content">
+                    <a href="/"><button className='logout-button'>Logout</button></a>
+                </div>
+            </nav>
+        </div>
         </>
     );
 }
