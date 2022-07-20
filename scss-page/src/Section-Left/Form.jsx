76d189a940d9d7fa90c3529a3b2a24d0bfd3640c
Usama Ashraf <usama@celerify.com>
2022-07-20 12:56:59 +0500

    form compoenent

---
+++ scss-page/src/Section-Left/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Section-Left/Form.scss';
+
+const Form = (props) => {
+    return (
+        <>
+        <form action="#">
+            <div className='left-input-field'>
+                <input type="text" className='left-input-field' placeholder= {props.placeholder}/>
+                <div className={props.class}><i className= {props.icon}></i> {props.name} </div>
+                <label htmlFor="self-introduction">{props.label} </label>
+            </div>
+        </form>
+        </>
+    );
+}
+
+export default Form;
