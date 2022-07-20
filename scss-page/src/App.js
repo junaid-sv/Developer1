1d12c5469627a833d080119c3595792969d707a6
Usama Ashraf <usama@celerify.com>
2022-07-20 12:54:15 +0500

    left-component

--- scss-page/src/App.js
+++ scss-page/src/App.js
@@ -1,11 +1,15 @@
+import logo from './logo.svg';
 import React from 'react';
+import Left from './Section-Left/Left';
 import Navbar from './Navbar/Navbar';
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
+      <Left/>
     </>
   );
 }
+
 export default App;
