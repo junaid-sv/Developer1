9cb35c15afbbad9cb701878346758b7a9e8a9fc2
Usama Ashraf <usama@celerify.com>
2022-07-21 17:17:01 +0500

    optimize

--- scss-page/src/Section-Right/Right.jsx
+++ scss-page/src/Section-Right/Right.jsx
@@ -5,33 +5,31 @@ const Right = () => {
     return (
         <>
             <aside>
-                <div className="right-main">
-                    <div className="right-container-top">
-                        <div className="right-title-top">
-                            <h4>hope condition</h4>
-                            <p><i className="fa-solid fa-pen"></i> edit</p>
-                        </div>
-                        <div className="right-content-top">
-                            <p>Occupation--</p>
-                            <p>On duty -----</p>
-                            <p>Work Style--</p>
-                            <p>Facility ----- <br/>form</p>
-                            <p>annual   ----<br/>income</p>
-                        </div>
+                <div className="right-container-top">
+                    <div className="right-title-top">
+                        <h4>hope condition</h4>
+                        <a href='/'><i className="fa-solid fa-pen"></i> edit</a>
                     </div>
-                    <div className="right-container-bottom">
-                        <div className="right-title-bottom">
-                            <h4><span> Personal Information</span> (private)</h4>
-                            <p><i className="fa-solid fa-pen"></i> edit</p>
-                        </div>
-                        <div className="right-content-bottom">
-                            <a href="/"><i class="fa-solid fa-user"></i>  --- ---</a>
-                            <a href="/">---</a>
-                            <a href="/"><i class="fa-solid fa-envelope"></i> junaidahmad@southvillesol.com</a>
-                            <a href="/"><i class="fa-solid fa-phone-flip"></i> ---</a>
-                            <a href="/"><i class="fa-solid fa-house"></i> ---</a>
-                            <a href="/"><i class="fa-solid fa-building-columns"></i> <span> Transfer account not set</span></a>
-                        </div>
+                    <div className="right-content-top">
+                        <p>Occupation--</p>
+                        <p>On duty -----</p>
+                        <p>Work Style--</p>
+                        <p>Facility ----- <br/>form</p>
+                        <p>annual   ----<br/>income</p>
+                    </div>
+                </div>
+                <div className="right-container-bottom">
+                    <div className="right-title-bottom">
+                        <h4><span> Personal Information</span> (private)</h4>
+                        <a href='/'><i className="fa-solid fa-pen"></i> edit</a>
+                    </div>
+                    <div className="right-content-bottom">
+                        <a href="/"><i class="fa-solid fa-user"></i>  --- ---</a>
+                        <a href="/">---</a>
+                        <a href="/"><i class="fa-solid fa-envelope"></i>  junaidahmad@southvillesol.com</a>
+                        <a href="/"><i class="fa-solid fa-phone-flip"></i> ---</a>
+                        <a href="/"><i class="fa-solid fa-house"></i> ---</a>
+                        <a href="/"><i class="fa-solid fa-building-columns"></i> <span> Transfer account not set</span></a>
                     </div>
                 </div>
             </aside>
