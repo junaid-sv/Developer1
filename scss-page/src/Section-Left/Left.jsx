a149597fef4218865ed365a3400537d04fe22f15
Usama Ashraf <usama@celerify.com>
2022-07-25 11:37:45 +0500

    indenatation

--- scss-page/src/Section-Left/Left.jsx
+++ scss-page/src/Section-Left/Left.jsx
@@ -8,41 +8,41 @@ const Left = () => {
         <>
             <main className="left-container">
                 <div className="left-UI">
-                <Card>
-                    <section className='left-center'>
+                    <Card>
+                        <section className='left-center'>
                             <h2 className='left-heading'>N</h2>
-                        <div className="left-content">
-                            <h2 className='font'><span className='left-name'>Nabeel</span>  Ahmad  00 </h2>
-                            <a href='/' className='font'><i className="fa-solid fa-location-dot"></i>Location area</a>
-                            <a href='/ ' className='font'><i className="fa-solid fa-briefcase"></i>Current Location</a>
-                            <p>Experienced occupation: ---</p>
-                        </div>
-                        <div className='left-edit'>
-                            <a href='/'><i class="fa-solid fa-pen"></i> edit</a>
-                        </div>
-                    </section>
-                    <Form 
-                        placeholder ='Self-introduction'
-                        name ='edit'
-                        label = 'Fill in self-introduction'
-                        class ='edit'
-                        icon = 'fa-solid fa-pen'
-                    />
-                    <Form 
-                        placeholder ='Working history'
-                        name ='addition'
-                        label = 'Add working history'
-                        class ='addition'
-                        icon = 'fa-solid fa-square-plus'
-                    />
-                    <Form 
-                        placeholder ='Add qualifications'
-                        name ='addition'
-                        label = 'Add qualification'
-                        class ='addition'
-                        icon = 'fa-solid fa-square-plus'
-                    />
-                </Card>
+                            <div className="left-content">
+                                <h2 className='font'><span className='left-name'>Nabeel</span>  Ahmad  00 </h2>
+                                <a href='/' className='font'><i className="fa-solid fa-location-dot"></i>Location area</a>
+                                <a href='/ ' className='font'><i className="fa-solid fa-briefcase"></i>Current Location</a>
+                                <p>Experienced occupation: ---</p>
+                            </div>
+                            <div className='left-edit'>
+                                <a href='/'><i class="fa-solid fa-pen"></i> edit</a>
+                            </div>
+                        </section>
+                        <Form
+                            placeholder='Self-introduction'
+                            name='edit'
+                            label='Fill in self-introduction'
+                            class='edit'
+                            icon='fa-solid fa-pen'
+                        />
+                        <Form
+                            placeholder='Working history'
+                            name='addition'
+                            label='Add working history'
+                            class='addition'
+                            icon='fa-solid fa-square-plus'
+                        />
+                        <Form
+                            placeholder='Add qualifications'
+                            name='addition'
+                            label='Add qualification'
+                            class='addition'
+                            icon='fa-solid fa-square-plus'
+                        />
+                    </Card>
                 </div>
             </main>
         </>
