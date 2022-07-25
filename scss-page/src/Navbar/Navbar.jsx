65a642f01860e68ecbe0840602cca8a744fce0e5
Usama Ashraf <usama@celerify.com>
2022-07-25 17:18:37 +0500

    check

--- scss-page/src/Navbar/Navbar.jsx
+++ scss-page/src/Navbar/Navbar.jsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import './Navbar.scss';
+import React from "react";
+import "./Navbar.scss";
 
 const Navbar = () => {
-    return (
-        <>
-        <div className="navbar">
-            <a href="/" className='logo'>Wecare</a>
-                <nav className='nav-links'>
-                    <ul>
-                        <li><a href="/">Job search</a></li>
-                        <li><a href="/">be on one's mind!</a></li>
-                        <li><a href="/">message</a></li>
-                        <li><a href="/" className='active'>My page</a></li>
-                        <li><a href="/"><button className='link-button'>N</button></a></li>
-                    </ul>
-                    <div className="logout-content">
-                        <a href="/"><button className='logout-button'>Logout</button></a>
-                    </div>
-                </nav>
-            </div>
-        </>
-    );
-}
+  return (
+    <>
+      <div className="navbar">
+        <a href="/" className="logo">
+          Wecare
+        </a>
+        <nav>
+          <ul>
+            <li>Job search</li>
+            <li>
+              <a href="/">be on one's mind!</a>
+            </li>
+            <li>
+              <a href="/">message</a>
+            </li>
+            <li>
+              <a href="/" className="active">
+                My page
+              </a>
+            </li>
+            <li>
+              <a href="/">
+                <button className="link-button">N</button>
+              </a>
+            </li>
+          </ul>
+          <a href="/">
+            <button className="logout-button">Logout</button>
+          </a>
+        </nav>
+      </div>
+    </>
+  );
+};
 export default Navbar;
